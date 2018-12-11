7198389082fbe4e0e1ec56f18578cba9e2e90701
vitorchioda <vitorchioda@hotmail.com>
2018-12-11 18:47:52 -0200

    escolarium

---
+++ src/main/java/com/vitorchioda/escolariun/EscolariumApplication.java
@@ -0,0 +1,12 @@
+package com.vitorchioda.escolariun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolariumApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vitorchioda/escolariun/EscolariumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vitorchioda.escolariun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscolariumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
